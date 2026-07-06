919351c85af7a74076cc8f117fe7e8b545dab6c9
Aleksander Gornik <alexbond008@users.noreply.github.com>
2026-07-06 12:23:59 +0100

    Initial commit from template

---
+++ src/client/java/supercreepermod/modid/client/SuperCreeperModClient.java
@@ -0,0 +1,10 @@
+package supercreepermod.modid.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SuperCreeperModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
